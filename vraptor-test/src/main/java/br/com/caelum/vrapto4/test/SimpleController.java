5b47d736c3ae6f719dcc3a9e1fe4ddc4b5aeca66
Alberto Souza <alots.ssa@gmail.com>
2013-06-20 14:24:29 -0300

    creating simple controllers for vraptor-test

---
+++ vraptor-test/src/main/java/br/com/caelum/vrapto4/test/SimpleController.java
@@ -0,0 +1,20 @@
+package br.com.caelum.vrapto4.test;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor4.Controller;
+import static br.com.caelum.vraptor.view.Results.http;
+
+@Controller
+public class SimpleController {
+	
+	@Inject
+	private Result result;
+	
+	public void teste() throws IOException{
+		result.use(http()).body("<html><body>Olha olha ai</body></html>");
+	}
+}

a91a6432999c14040226066d01dd395358bf9b5a
Alberto Souza <alots.ssa@gmail.com>
2013-07-01 18:50:23 -0300

    improve test code

--- vraptor-core/src/test/java/br/com/caelum/vraptor4/interceptor/PackageAcceptorTest.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor4/interceptor/PackageAcceptorTest.java
@@ -4,7 +4,10 @@ import java.lang.reflect.AnnotatedElement;
 
 import net.vidageek.mirror.dsl.Mirror;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import br.com.caelum.vraptor4.controller.BeanClass;
 import br.com.caelum.vraptor4.controller.ControllerInstance;
@@ -12,64 +15,72 @@ import br.com.caelum.vraptor4.controller.ControllerInstance;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class PackageAcceptorTest {
+	
+	private @Mock ControllerInstance controllerInstance;
+	private @Mock BeanClass beanClass;	
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		when(controllerInstance.getBeanClass()).thenReturn(beanClass);
+	}	
 
 	@Test
 	public void shouldAcceptSamePackage() throws Exception {
 		PackagesAcceptor acceptor = new PackagesAcceptor();
-		ControllerInstance controllerInstance = mock(ControllerInstance.class);
-		BeanClass beanClass = mock(BeanClass.class);
-		when(beanClass.getPackageName()).thenReturn("br.com.caelum");
-		when(controllerInstance.getBeanClass()).thenReturn(beanClass);
 		AcceptsForPackages annotation = new Mirror()
 		.on((AnnotatedElement) SinglePackageInterceptor.class)
 		.reflect().annotation(AcceptsForPackages.class);		
+		
+		when(beanClass.getPackageName()).thenReturn("br.com.caelum");
+		
 		acceptor.initialize(annotation);
+		
 		assertTrue(acceptor.validate(null, controllerInstance));
 	}
 	
 	@Test
 	public void shouldAcceptSamePackageInMultiple() throws Exception {
 		PackagesAcceptor acceptor = new PackagesAcceptor();
-		ControllerInstance controllerInstance = mock(ControllerInstance.class);
-		BeanClass beanClass = mock(BeanClass.class);
-		when(beanClass.getPackageName()).thenReturn("br.com.caelum.controller.report");
-		when(controllerInstance.getBeanClass()).thenReturn(beanClass);
 		AcceptsForPackages annotation = new Mirror()
 		.on((AnnotatedElement) MultiplePackageInterceptor.class)
-		.reflect().annotation(AcceptsForPackages.class);		
+		.reflect().annotation(AcceptsForPackages.class);
+		
+		when(beanClass.getPackageName()).thenReturn("br.com.caelum.controller.report");
+		
 		acceptor.initialize(annotation);
+		
 		assertTrue(acceptor.validate(null, controllerInstance));
 	}
 	
 	@Test
 	public void shouldNotAcceptDifferentPackage() throws Exception {
 		PackagesAcceptor acceptor = new PackagesAcceptor();
-		ControllerInstance controllerInstance = mock(ControllerInstance.class);
-		BeanClass beanClass = mock(BeanClass.class);
-		when(beanClass.getPackageName()).thenReturn("br.com.caelum.controller.module");
-		when(controllerInstance.getBeanClass()).thenReturn(beanClass);
 		AcceptsForPackages annotation = new Mirror()
 		.on((AnnotatedElement) MultiplePackageInterceptor.class)
 		.reflect().annotation(AcceptsForPackages.class);		
+		
+		when(beanClass.getPackageName()).thenReturn("br.com.caelum.controller.module");
+		
 		acceptor.initialize(annotation);
+		
 		assertFalse(acceptor.validate(null, controllerInstance));
 	}
 	
 	@Test
 	public void shouldAcceptSubPackage() throws Exception {
 		PackagesAcceptor acceptor = new PackagesAcceptor();
-		ControllerInstance controllerInstance = mock(ControllerInstance.class);
-		BeanClass beanClass = mock(BeanClass.class);
-		when(beanClass.getPackageName()).thenReturn("br.com.caelum.controller.report");
-		when(controllerInstance.getBeanClass()).thenReturn(beanClass);
 		AcceptsForPackages annotation = new Mirror()
 		.on((AnnotatedElement) SubPackageInterceptor.class)
-		.reflect().annotation(AcceptsForPackages.class);		
+		.reflect().annotation(AcceptsForPackages.class);
+		
+		when(beanClass.getPackageName()).thenReturn("br.com.caelum.controller.report");
+		
 		acceptor.initialize(annotation);
+		
 		assertTrue(acceptor.validate(null, controllerInstance));
 	}
 	

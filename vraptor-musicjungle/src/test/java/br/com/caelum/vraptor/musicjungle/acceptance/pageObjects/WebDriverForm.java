f782b7e7f1ce089d59020f7a3e67dfdcfb1cae1f
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-07-03 18:28:41 -0300

    Adding WebDriverForm

---
+++ vraptor-musicjungle/src/test/java/br/com/caelum/vraptor/musicjungle/acceptance/pageObjects/WebDriverForm.java
@@ -0,0 +1,35 @@
+package br.com.caelum.vraptor.musicjungle.acceptance.pageObjects;
+
+import static org.openqa.selenium.By.tagName;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class WebDriverForm {
+
+	private WebDriver driver;
+
+	public WebDriverForm(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public WebDriverForm select(By by, String value) {
+		
+		WebElement select = driver.findElement(by);
+		List<WebElement> options = select.findElements(tagName("option"));
+		
+		for (WebElement option : options) {
+			String optionValue = option.getAttribute("value");
+			if (optionValue.equals(value)) option.click();
+		}
+		return this;
+	}
+
+	public void submit() {
+		driver.findElement(By.tagName("form")).submit();
+	}
+
+}

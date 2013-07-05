eb2cd793a04d6ce6ab71b88dc88262123e561783
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-07-05 17:21:05 -0300

    Using form instead driver on PageForm

--- vraptor-musicjungle/src/acceptance/java/br/com/caelum/vraptor/musicjungle/acceptance/pages/PageForm.java
+++ vraptor-musicjungle/src/acceptance/java/br/com/caelum/vraptor/musicjungle/acceptance/pages/PageForm.java
@@ -1,5 +1,6 @@
 package br.com.caelum.vraptor.musicjungle.acceptance.pages;
 
+import static org.openqa.selenium.By.name;
 import static org.openqa.selenium.By.tagName;
 
 import java.util.List;
@@ -10,16 +11,17 @@ import org.openqa.selenium.WebElement;
 
 public class PageForm {
 
-	private WebDriver driver;
+	private WebElement form;
 
-	public PageForm(WebDriver driver) {
-		this.driver = driver;
+	public PageForm(WebDriver driver, By by) {
+		this.form = driver.findElement(by);
 	}
 
 	public PageForm select(By by, String value) {
 		
-		WebElement select = driver.findElement(by);
-		List<WebElement> options = select.findElements(tagName("option"));
+		WebElement select = form.findElement(by);
+		By byOption = tagName("option");
+		List<WebElement> options = select.findElements(byOption);
 		
 		for (WebElement option : options) {
 			String optionValue = option.getAttribute("value");
@@ -27,9 +29,15 @@ public class PageForm {
 		}
 		return this;
 	}
+	
+	public PageForm input(String name, String value) {
+		WebElement element = form.findElement(name(name));
+		element.sendKeys(value);
+		return this;
+	}
 
-	public void submit() {
-		driver.findElement(By.tagName("form")).submit();
+	public void submitForm() {
+		form.submit();
 	}
 
 }

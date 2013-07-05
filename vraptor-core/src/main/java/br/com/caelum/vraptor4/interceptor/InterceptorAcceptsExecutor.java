7a6cf271bfc4991bcaaf8095a26deae6b668c161
Alberto Souza <alots.ssa@gmail.com>
2013-07-04 22:30:02 -0300

    extracting resolution of parameters

--- vraptor-core/src/main/java/br/com/caelum/vraptor4/interceptor/InterceptorAcceptsExecutor.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor4/interceptor/InterceptorAcceptsExecutor.java
@@ -1,9 +1,5 @@
 package br.com.caelum.vraptor4.interceptor;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-
 import br.com.caelum.vraptor.ioc.Container;
 import br.com.caelum.vraptor4.Accepts;
 
@@ -11,19 +7,21 @@ public class InterceptorAcceptsExecutor {
 
 	private StepInvoker stepInvoker;
 	private Container container;
+	private InterceptorMethodParameterResolver parameterResolver;
 
 	public InterceptorAcceptsExecutor(StepInvoker stepInvoker,
 			Container container) {
 		super();
 		this.stepInvoker = stepInvoker;
 		this.container = container;
+		this.parameterResolver = new InterceptorMethodParameterResolver(stepInvoker, container);
 	}
 
 	public boolean execute(Object interceptor) {
 		boolean interceptorAccepts = true;
 		Object returnObject = stepInvoker.tryToInvoke(interceptor,
 				Accepts.class, new NoStackParameterSignatureAcceptor(),
-				parametersFor(Accepts.class, interceptor, container));
+				parameterResolver.parametersFor(Accepts.class, interceptor));
 		if (returnObject != null) {
 			if (!returnObject.getClass().equals(Boolean.class)) {
 				throw new IllegalStateException(
@@ -32,21 +30,5 @@ public class InterceptorAcceptsExecutor {
 			interceptorAccepts = (Boolean) returnObject;
 		}
 		return interceptorAccepts;
-	}
-
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	private Object[] parametersFor(Class<? extends Annotation> step,
-			Object interceptor, Container container) {
-		Method methodToInvoke = stepInvoker.findMethod(step, interceptor);
-		if (methodToInvoke == null)
-			return new Object[] {};
-		Class<?>[] parameterTypes = methodToInvoke.getParameterTypes();
-		ArrayList parameters = new ArrayList();
-		for (Class<?> parameterType : parameterTypes) {
-			System.out.println(parameterType);
-			parameters.add(container.instanceFor(parameterType));
-		}
-		return parameters.toArray();
-
 	}
 }

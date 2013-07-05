abff695cebbc8c530f50912e36734815d8c4d57e
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-07-05 17:20:05 -0300

    login, logout and createUser methods

--- vraptor-musicjungle/src/acceptance/java/br/com/caelum/vraptor/musicjungle/acceptance/pages/LoginPage.java
+++ vraptor-musicjungle/src/acceptance/java/br/com/caelum/vraptor/musicjungle/acceptance/pages/LoginPage.java
@@ -1,7 +1,9 @@
 package br.com.caelum.vraptor.musicjungle.acceptance.pages;
 
-import org.openqa.selenium.WebDriver;
+import static org.openqa.selenium.By.cssSelector;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
 
@@ -9,6 +11,28 @@ public class LoginPage {
 
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
+	}
+
+	public void loginAsUser(String user) {
+		By cssSelector = cssSelector("form:nth-child(1)");
+		PageForm form = new PageForm(driver, cssSelector);
+		createUser(user);
+		form.input("login", user);
+		form.input("password", user);
+		form.submitForm();
+	}
+
+	private void createUser(String value) {
+		By cssSelector = cssSelector("form:nth-child(2)");
+		PageForm form = new PageForm(driver, cssSelector);
+		form.input("user.name", value);
+		form.input("user.login", value);
+		form.input("user.password", value);
+		form.submitForm();
+	}
+
+	public void logout() {
+		driver.findElement(By.linkText("Logout"));
 	}
 	
 }

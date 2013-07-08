96b908a339eeb4cc75f4ded558427ea0e512aaa7
Alberto Souza <alots.ssa@gmail.com>
2013-07-08 13:59:32 -0300

    extracted acceptssignatureacceptor

---
+++ vraptor-core/src/main/java/br/com/caelum/vraptor4/interceptor/InternalAcceptsSignature.java
@@ -0,0 +1,33 @@
+package br.com.caelum.vraptor4.interceptor;
+
+import java.lang.reflect.Method;
+
+public class InternalAcceptsSignature implements SignatureAcceptor {
+
+	private SignatureAcceptor signatureAcceptor;
+	private String errorMessage = "@Accepts method must return boolean";
+
+	public InternalAcceptsSignature(SignatureAcceptor signatureAcceptor) {
+		super();
+		this.signatureAcceptor = signatureAcceptor;
+	}
+
+	@Override
+	public boolean accepts(Method method) {
+		if (!signatureAcceptor.accepts(method)) {
+			this.errorMessage = signatureAcceptor.errorMessage();
+			return false;
+		}
+		if (!method.getReturnType().equals(Boolean.class)
+				&& !method.getReturnType().equals(boolean.class)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String errorMessage() {
+		return this.errorMessage;
+	}
+
+}

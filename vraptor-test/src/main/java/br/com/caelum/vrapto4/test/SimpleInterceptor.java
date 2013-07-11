e62228144f4dc9308e16c522daf30a47ba29c897
Alberto Souza <alots.ssa@gmail.com>
2013-07-11 20:53:17 -0300

    ordering interceptor

--- vraptor-test/src/main/java/br/com/caelum/vrapto4/test/SimpleInterceptor.java
+++ vraptor-test/src/main/java/br/com/caelum/vrapto4/test/SimpleInterceptor.java
@@ -1,12 +1,13 @@
 package br.com.caelum.vrapto4.test;
 
 import br.com.caelum.vraptor.Intercepts;
+import br.com.caelum.vraptor.interceptor.InstantiateInterceptor;
 import br.com.caelum.vraptor4.AfterCall;
 import br.com.caelum.vraptor4.AroundCall;
 import br.com.caelum.vraptor4.BeforeCall;
 import br.com.caelum.vraptor4.interceptor.SimpleInterceptorStack;
 
-@Intercepts
+@Intercepts(after=InstantiateInterceptor.class)
 public class SimpleInterceptor {
 
 	@BeforeCall

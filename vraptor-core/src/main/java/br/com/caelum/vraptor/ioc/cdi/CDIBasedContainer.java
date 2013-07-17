61befa53d0df7b62d262d4d8229afb0872b3d0fe
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-07-17 10:00:58 -0300

    Removing implements ComponentRegistry form CDIBasedContainer

--- vraptor-core/src/main/java/br/com/caelum/vraptor/ioc/cdi/CDIBasedContainer.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/ioc/cdi/CDIBasedContainer.java
@@ -5,10 +5,9 @@ import javax.enterprise.inject.spi.CDI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import br.com.caelum.vraptor.ComponentRegistry;
 import br.com.caelum.vraptor.ioc.Container;
 
-public class CDIBasedContainer implements Container, ComponentRegistry {
+public class CDIBasedContainer implements Container {
 
 	private static final Logger logger = LoggerFactory.getLogger(CDIBasedContainer.class);
 
@@ -25,19 +24,6 @@ public class CDIBasedContainer implements Container, ComponentRegistry {
 	@Override
 	public <T> boolean canProvide(Class<T> type) {
 		return !CDI.current().select(type).isUnsatisfied();
-	}
-
-	@Override
-	public void register(Class<?> requiredType, Class<?> componentType) {
-		// it is not possible using CDI. We can only registrer on the container
-		// startup.
-		logger.debug("Should register " + requiredType + " associated with "
-				+ componentType);
-	}
-
-	@Override
-	public void deepRegister(Class<?> componentType) {
-		logger.debug("Should deep register " + componentType);
 	}
 
 }

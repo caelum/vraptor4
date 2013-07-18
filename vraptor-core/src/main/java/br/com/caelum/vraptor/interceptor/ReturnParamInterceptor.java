f6a88157dd4333690c95f50c7cd3fb02d8b7020e
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-07-18 09:59:15 -0300

    Using BeforeCall, cutting unused params/code

--- vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/ReturnParamInterceptor.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/ReturnParamInterceptor.java
@@ -2,12 +2,11 @@ package br.com.caelum.vraptor.interceptor;
 
 import javax.inject.Inject;
 
-import br.com.caelum.vraptor.InterceptionException;
 import br.com.caelum.vraptor.Intercepts;
 import br.com.caelum.vraptor.Result;
-import br.com.caelum.vraptor.core.InterceptorStack;
 import br.com.caelum.vraptor.core.MethodInfo;
 import br.com.caelum.vraptor.http.ParameterNameProvider;
+import br.com.caelum.vraptor4.BeforeCall;
 import br.com.caelum.vraptor4.controller.ControllerMethod;
 
 @Intercepts(
@@ -20,8 +19,8 @@ public class ReturnParamInterceptor {
 	@Inject private Result result;
 	@Inject private ParameterNameProvider nameProvider;
 
-	public void intercept(InterceptorStack stack, ControllerMethod method,
-			Object instance) throws InterceptionException {
+	@BeforeCall
+	public void intercept(ControllerMethod method) {
 
 		Object[] parameters = info.getParameters();
 		String[] names = nameProvider.parameterNamesFor(method.getMethod());
@@ -29,6 +28,5 @@ public class ReturnParamInterceptor {
 		for(int i=0; i< names.length; i++) {
 			result.include(names[i], parameters[i]);
 		}
-		stack.next(method, instance);
 	}
 }

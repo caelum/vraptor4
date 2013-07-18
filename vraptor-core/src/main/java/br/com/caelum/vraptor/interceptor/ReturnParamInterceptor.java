6ff3c1d5dffc00422309b5977f2d3db61af9f164
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-07-18 15:16:20 -0300

    Using constructor injection

--- vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/ReturnParamInterceptor.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/ReturnParamInterceptor.java
@@ -11,18 +11,35 @@ import br.com.caelum.vraptor.http.ParameterNameProvider;
 import br.com.caelum.vraptor4.BeforeCall;
 import br.com.caelum.vraptor4.controller.ControllerMethod;
 
-@Intercepts(after=ParametersInstantiatorInterceptor.class)
+@Intercepts(
+	after=ParametersInstantiatorInterceptor.class
+)
 public class ReturnParamInterceptor {
 
-	@Inject private MethodInfo info;
-	@Inject private Result result;
-	@Inject private ParameterNameProvider nameProvider;
-	@Inject private ControllerMethod cmethod;
+	private MethodInfo info;
+	private Result result;
+	private ParameterNameProvider nameProvider;
+	private ControllerMethod controllerMethod;
+
+	@Deprecated // CDI eyes only
+	public ReturnParamInterceptor() {}
+
+	@Inject
+	public ReturnParamInterceptor(MethodInfo info,
+			Result result, ParameterNameProvider nameProvider,
+			ControllerMethod controllerMethod) {
+
+		this.info = info;
+		this.result = result;
+		this.nameProvider = nameProvider;
+		this.controllerMethod = controllerMethod;
+	}
 
 	@BeforeCall
 	public void intercept() {
+
 		Object[] parameters = info.getParameters();
-		Method method = cmethod.getMethod();
+		Method method = controllerMethod.getMethod();
 		String[] names = nameProvider.parameterNamesFor(method);
 
 		for(int i=0; i< names.length; i++) {

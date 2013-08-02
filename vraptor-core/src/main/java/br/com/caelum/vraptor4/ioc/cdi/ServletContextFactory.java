372d64b498627e0ba745c91bba45146e573f8d8e
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-08-02 20:07:37 -0300

    Alternatives with LIBRARY_BEFORE Priority closes #23

--- vraptor-core/src/main/java/br/com/caelum/vraptor4/ioc/cdi/ServletContextFactory.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor4/ioc/cdi/ServletContextFactory.java
@@ -4,21 +4,22 @@ import javax.annotation.Priority;
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.Alternative;
 import javax.enterprise.inject.Produces;
+import javax.interceptor.Interceptor;
 import javax.servlet.ServletContext;
 
 import br.com.caelum.vraptor4.ioc.ApplicationScoped;
 
 @ApplicationScoped
 @Alternative
-@Priority(1000)
+@Priority(Interceptor.Priority.LIBRARY_BEFORE)
 public class ServletContextFactory{
 
 	private ServletContext context;
-	
+
 	public void observesContext(@Observes ServletContext context){
 		this.context = context;
 	}
-	
+
 	@Produces
 	@javax.enterprise.context.ApplicationScoped
 	public ServletContext getInstance(){

dc2b7b961d586b317272e31bb5282e6b24aa7741
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-09-09 18:35:26 -0300

    adding @alternative

--- vraptor-core/src/test/java/br/com/caelum/vraptor/ioc/FakeInterceptorHandlerFactory.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/ioc/FakeInterceptorHandlerFactory.java
@@ -1,15 +1,16 @@
 package br.com.caelum.vraptor.ioc;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Alternative;
 
 import br.com.caelum.vraptor.core.InterceptorHandler;
 import br.com.caelum.vraptor.core.InterceptorHandlerFactory;
 
 @ApplicationScoped
+@Alternative
 public class FakeInterceptorHandlerFactory implements InterceptorHandlerFactory{
 
 	public InterceptorHandler handlerFor(Class<?> type) {
 		return null;
 	}
-
 }

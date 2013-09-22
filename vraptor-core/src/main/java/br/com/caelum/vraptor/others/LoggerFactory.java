7a991c1652423cefd198577a5d941b5689aa663b
Otávio Garcia <otavio@otavio.com.br>
2013-09-22 11:30:06 -0300

    Logger should be dependant

--- vraptor-core/src/main/java/br/com/caelum/vraptor/others/LoggerFactory.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/others/LoggerFactory.java
@@ -1,6 +1,5 @@
 package br.com.caelum.vraptor.others;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 
@@ -14,7 +13,7 @@ import org.slf4j.Logger;
  */
 public class LoggerFactory {
 	
-	@Produces @ApplicationScoped
+	@Produces
 	public Logger getLogger(InjectionPoint ip){
 		Class<?> clazz = ip.getMember().getDeclaringClass();
 		return org.slf4j.LoggerFactory.getLogger(clazz);

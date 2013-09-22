7b80f968f78fa0a851a84b93decf90aa9ddea673
Otávio Garcia <otavio@otavio.com.br>
2013-09-22 01:10:51 -0300

    Restoring LoggerFactory and changing scope

---
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/others/LoggerFactory.java
@@ -0,0 +1,22 @@
+package br.com.caelum.vraptor.others;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+
+import org.slf4j.Logger;
+
+/**
+ * Produces an instance of {@link org.slf4j.LoggerFactory}.
+ * 
+ * @author Rodrigo Turini
+ * @since 4.0.0
+ */
+public class LoggerFactory {
+	
+	@Produces @ApplicationScoped
+	public Logger getLogger(InjectionPoint ip){
+		Class<?> clazz = ip.getMember().getDeclaringClass();
+		return org.slf4j.LoggerFactory.getLogger(clazz);
+	}
+}

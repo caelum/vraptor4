85e24c7094501d324e2681c532a963354c944e5f
Alberto Souza <alots.ssa@gmail.com>
2013-09-28 12:18:16 -0300

    caching instance object instead of looking for every time

--- vraptor-core/src/main/java/br/com/caelum/vraptor/ioc/cdi/CDIBasedContainer.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/ioc/cdi/CDIBasedContainer.java
@@ -3,11 +3,27 @@ package br.com.caelum.vraptor.ioc.cdi;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Instance;
 import javax.enterprise.inject.spi.CDI;
+import javax.inject.Inject;
 
+import br.com.caelum.vraptor.cache.LRU;
+import br.com.caelum.vraptor.cache.VRaptorCache;
 import br.com.caelum.vraptor.ioc.Container;
 
 @ApplicationScoped
+@SuppressWarnings("rawtypes")
 public class CDIBasedContainer implements Container {
+	
+	private VRaptorCache<String,Instance> cache;
+
+	@Deprecated
+	public CDIBasedContainer() {
+	}
+	
+	@Inject
+	public CDIBasedContainer(@LRU(capacity=1000) VRaptorCache<String, Instance> cache) {
+		super();
+		this.cache = cache;
+	}
 
 	@Override
 	public <T> T instanceFor(Class<T> type) {
@@ -20,6 +36,11 @@ public class CDIBasedContainer implements Container {
 	}
 
 	private <T> Instance<T> selectFromContainer(Class<T> type) {
-		return CDI.current().select(type);
+		String className = type.getCanonicalName();
+		if(cache.get(className)==null){
+			Instance<T> instance = CDI.current().select(type);
+			cache.put(className,instance);
+		}
+		return cache.get(className);
 	}
 }

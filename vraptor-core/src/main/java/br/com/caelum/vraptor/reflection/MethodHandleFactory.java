03a8c3090949003a8dbdd0fae2261fed6a7cee94
Mario Amaral <mario.fts@gmail.com>
2013-10-01 18:00:33 -0300

    javadoc

--- vraptor-core/src/main/java/br/com/caelum/vraptor/reflection/MethodHandleFactory.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/reflection/MethodHandleFactory.java
@@ -2,8 +2,8 @@ package br.com.caelum.vraptor.reflection;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
 import java.lang.invoke.MethodHandles.Lookup;
+import java.lang.invoke.MethodType;
 import java.lang.reflect.Method;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -18,6 +18,14 @@ import com.headius.invokebinder.Binder;
 @ApplicationScoped
 public class MethodHandleFactory {
 
+	
+	/**
+	 * Creates a MethodHandle for some method.
+	 * 
+	 * @param type The method receiver
+	 * @param method The reflection representation of the method
+	 * @return The MethodHandle representation
+	 */
 	public MethodHandle create(Class<?> type, Method method) {
 		if (method == null) {
 			return null;
@@ -41,6 +49,12 @@ public class MethodHandleFactory {
 
 	}
 
+	/**
+	 * Creates a generic Binder for methods
+	 * @param method
+	 * @param numberOfArguments
+	 * @return
+	 */
 	private Binder createBinder(Method method, int numberOfArguments) {
 		return Binder.from(Object.class, Object.class, Object[].class).spread(numberOfArguments);
 	}

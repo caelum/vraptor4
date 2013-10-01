be2e7809f66d5c4f2dccf0f76477281063e3b54b
Mario Amaral <mario.fts@gmail.com>
2013-10-01 18:02:09 -0300

    extracting parameter

--- vraptor-core/src/test/java/br/com/caelum/vraptor/core/DefaultInterceptorHandlerFactoryTest.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/core/DefaultInterceptorHandlerFactoryTest.java
@@ -1,34 +1,39 @@
 package br.com.caelum.vraptor.core;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+
+import br.com.caelum.vraptor.cache.VRaptorCache;
+import br.com.caelum.vraptor.cache.VRaptorDefaultCache;
+import br.com.caelum.vraptor.factory.Factories;
+import br.com.caelum.vraptor.interceptor.AspectStyleInterceptorHandler;
+import br.com.caelum.vraptor.interceptor.Interceptor;
+import br.com.caelum.vraptor.interceptor.InterceptorMethodParametersResolver;
+import br.com.caelum.vraptor.interceptor.StepInvoker;
+import br.com.caelum.vraptor.ioc.Container;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.sameInstance;
 
 import static org.junit.Assert.assertThat;
 
-import org.junit.Before;
-import org.junit.Test;
-
-import br.com.caelum.vraptor.cache.VRaptorCache;
-import br.com.caelum.vraptor.cache.VRaptorDefaultCache;
-import br.com.caelum.vraptor.interceptor.AspectStyleInterceptorHandler;
-import br.com.caelum.vraptor.interceptor.Interceptor;
-import br.com.caelum.vraptor.interceptor.InterceptorMethodParametersResolver;
-import br.com.caelum.vraptor.interceptor.StepInvoker;
-import br.com.caelum.vraptor.ioc.Container;
-
 public class DefaultInterceptorHandlerFactoryTest {
 
 	private Container container;
 
 	private DefaultInterceptorHandlerFactory factory;
+	
+	private StepInvoker stepInvoker = Factories.createStepInvoker();
 
 	@Before
 	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		
 		InterceptorMethodParametersResolver parametersResolver = new InterceptorMethodParametersResolver(container);
 		
 		VRaptorCache<Class<?>, InterceptorHandler> cachedHandlers = new VRaptorDefaultCache<>();
-		factory = new DefaultInterceptorHandlerFactory(container, new StepInvoker(), parametersResolver, cachedHandlers);
+		factory = new DefaultInterceptorHandlerFactory(container, stepInvoker, parametersResolver, cachedHandlers);
 	}
 
 	static interface RegularInterceptor extends Interceptor {}

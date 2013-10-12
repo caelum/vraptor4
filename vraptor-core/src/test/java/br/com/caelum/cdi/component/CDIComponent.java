ae17f3fa06c8c4643a3dcd6efe9c946621dfe265
Otávio Garcia <otavio@otavio.com.br>
2013-10-11 22:40:34 -0300

    Removing more code

--- vraptor-core/src/test/java/br/com/caelum/cdi/component/CDIComponent.java
+++ vraptor-core/src/test/java/br/com/caelum/cdi/component/CDIComponent.java
@@ -3,19 +3,10 @@ package br.com.caelum.cdi.component;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 
-import br.com.caelum.vraptor.ioc.GenericContainerTest.MyRequestComponent;
 import br.com.caelum.vraptor.ioc.cdi.ComponentToBeProduced;
 
 public class CDIComponent{
 	
-	//CDI eyes only
-	@Deprecated
-	public CDIComponent() {
-	}
-	
-	public CDIComponent(MyRequestComponent component) {
-	}
-
 	@Produces
 	@RequestScoped
 	public ComponentToBeProduced getInstance() {		
--- vraptor-core/src/test/java/br/com/caelum/cdi/component/CDIControllerComponent.java
+++ vraptor-core/src/test/java/br/com/caelum/cdi/component/CDIControllerComponent.java
@@ -5,20 +5,4 @@ import br.com.caelum.vraptor.Controller;
 @Controller
 public class CDIControllerComponent {
 
-	private boolean initializedDepencies;
-
-	//CDI eyes only
-	@Deprecated
-	public CDIControllerComponent() {
-	}
-	
-	public CDIControllerComponent(CDIComponent component) {
-		this.initializedDepencies = true;
-	}
-
-	public boolean isInitializedDepencies() {
-		return initializedDepencies;
-	}
-	
-	
 }

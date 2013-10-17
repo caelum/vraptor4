7aa5cbeb741fdb1c590c29c6c89041876ba3e177
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-10-17 10:40:04 -0300

    adding MirrorList<Method> param on validateRule

--- vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/ValidationRule.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/ValidationRule.java
@@ -1,6 +1,10 @@
 package br.com.caelum.vraptor.interceptor;
 
+import java.lang.reflect.Method;
+
+import net.vidageek.mirror.list.dsl.MirrorList;
+
 public interface ValidationRule {
 
-	public void validate(Class<?> originalType);
+	public void validate(Class<?> originalType, MirrorList<Method> allMethods);
 }

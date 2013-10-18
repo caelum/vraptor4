9b18e69dfaebb374a48949b7e3f8579fe318aaab
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-10-18 13:57:56 -0300

    adding a test for valid case

--- vraptor-core/src/test/java/br/com/caelum/vraptor/interceptor/NoInterceptMethodsValidationRuleTest.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/interceptor/NoInterceptMethodsValidationRuleTest.java
@@ -4,22 +4,42 @@ import java.lang.reflect.Method;
 
 import net.vidageek.mirror.list.dsl.MirrorList;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import br.com.caelum.vraptor.AfterCall;
 import br.com.caelum.vraptor.InterceptionException;
 import br.com.caelum.vraptor.Intercepts;
 
 public class NoInterceptMethodsValidationRuleTest {
+
+	private StepInvoker stepInvoker;
 
 	@Intercepts
 	class SimpleInterceptor {
 		public void dummyMethodWithoutInterceptorAnnotations() {}
 	}
 
+	@Intercepts
+	class SimpleInterceptorWithCallableMethod {
+		@AfterCall public void afterCall() {}
+	}
+
+	@Before
+	public void setUp() {
+		this.stepInvoker = new StepInvoker(null);
+	}
+
 	@Test(expected=InterceptionException.class)
 	public void shoulThrowExceptionIfInterceptorDontHaveAnyCallableMethod() {
-		StepInvoker stepInvoker = new StepInvoker(null);
 		Class<?> type = SimpleInterceptor.class;
+		MirrorList<Method> allMethods = stepInvoker.findAllMethods(type);
+		new NoInterceptMethodsValidationRule(stepInvoker).validate(type, allMethods);
+	}
+
+	@Test
+	public void shoulWorksFineIfInterceptorHaveAtLeastOneCallableMethod() {
+		Class<?> type = SimpleInterceptorWithCallableMethod.class;
 		MirrorList<Method> allMethods = stepInvoker.findAllMethods(type);
 		new NoInterceptMethodsValidationRule(stepInvoker).validate(type, allMethods);
 	}

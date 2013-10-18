c43c15a114e2ed3992b7b9c908f30b6d9243b817
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-10-18 18:57:27 -0300

    removing container.instanceFor(...) using simpleIntercptorStack.next() instead

--- vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/StackNextExecutor.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/StackNextExecutor.java
@@ -2,18 +2,13 @@ package br.com.caelum.vraptor.interceptor;
 
 import javax.enterprise.inject.Vetoed;
 
-import br.com.caelum.vraptor.controller.ControllerInstance;
-import br.com.caelum.vraptor.controller.ControllerMethod;
-import br.com.caelum.vraptor.core.InterceptorStack;
-import br.com.caelum.vraptor.ioc.Container;
-
 @Vetoed
 public class StackNextExecutor implements StepExecutor<Void> {
 
-	private Container container;
+	private SimpleInterceptorStack simpleInterceptorStack;
 
-	public StackNextExecutor(Container container) {
-		this.container = container;
+	public StackNextExecutor(SimpleInterceptorStack simpleInterceptorStack) {
+		this.simpleInterceptorStack = simpleInterceptorStack;
 	}
 
 	@Override
@@ -23,11 +18,7 @@ public class StackNextExecutor implements StepExecutor<Void> {
 
 	@Override
 	public Void execute(Object interceptor) {
-		InterceptorStack stack = container.instanceFor(InterceptorStack.class);
-		ControllerMethod controllerMethod = container.instanceFor(ControllerMethod.class);
-		ControllerInstance controllerInstance = container.instanceFor(ControllerInstance.class);
-		stack.next(controllerMethod , controllerInstance.getController());
+		simpleInterceptorStack.next();
 		return null;
 	}
-
 }

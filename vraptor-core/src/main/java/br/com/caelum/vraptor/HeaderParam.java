cbd651a74ff09fc376ac5b69024e65b8d27a43b4
Otávio Garcia <otavio@otavio.com.br>
2013-10-22 20:11:47 -0200

    Adding docs about HeaderParam

--- vraptor-core/src/main/java/br/com/caelum/vraptor/HeaderParam.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/HeaderParam.java
@@ -5,10 +5,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
+/**
+ * Inject an HTTP header into a method parameter. 
+ */
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface HeaderParam {
 
+	/**
+	 * The name of header parameter.
+	 */
 	String value();
 }

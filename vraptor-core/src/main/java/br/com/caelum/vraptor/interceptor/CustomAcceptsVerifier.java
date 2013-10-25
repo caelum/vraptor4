cd1e0085ca04e8eae59e956cde6449386386a57f
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-10-25 13:49:45 -0200

    CustomAcceptsVerifier applicationScoped

--- vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/CustomAcceptsVerifier.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/CustomAcceptsVerifier.java
@@ -4,7 +4,8 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.AnnotatedElement;
 import java.util.List;
 
-import javax.enterprise.inject.Vetoed;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 import net.vidageek.mirror.dsl.Mirror;
 import net.vidageek.mirror.list.dsl.Matcher;
@@ -12,49 +13,48 @@ import br.com.caelum.vraptor.controller.ControllerInstance;
 import br.com.caelum.vraptor.controller.ControllerMethod;
 import br.com.caelum.vraptor.ioc.Container;
 
-@Vetoed
+@ApplicationScoped
 public class CustomAcceptsVerifier {
 
-	private static class AcceptsConstraintMatcher implements Matcher<Annotation> {
+	private final Container container;
 
-		@Override
-		public boolean accepts(Annotation element) {
-			return element.annotationType().isAnnotationPresent(AcceptsConstraint.class);
-		}
-
+	/**
+	 * @deprecated CDI eyes only
+	 */
+	public CustomAcceptsVerifier() {
+		this(null);
 	}
 
-	private ControllerInstance controllerInstance;
-	private Container container;
-	private Object interceptor;
-	private ControllerMethod controllerMethod;
-
-	public CustomAcceptsVerifier(ControllerMethod controllerMethod,ControllerInstance controllerInstance,
-			Container container, Object interceptor) {
-		this.controllerMethod = controllerMethod;
-		this.controllerInstance = controllerInstance;
+	@Inject
+	public CustomAcceptsVerifier(Container container) {
 		this.container = container;
-		this.interceptor = interceptor;
 	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public boolean isValid() {
-		List<Annotation> constraints = CustomAcceptsVerifier.getCustomAcceptsAnnotations(interceptor.getClass());
+	public boolean isValid(Object interceptor, ControllerMethod controllerMethod,
+			ControllerInstance controllerInstance, List<Annotation> constraints) {
+
 		for (Annotation annotation : constraints) {
 			AcceptsConstraint constraint = annotation.annotationType().getAnnotation(AcceptsConstraint.class);
 			Class<? extends AcceptsValidator<?>> validatorClass = constraint.value();
 			AcceptsValidator validator = container.instanceFor(validatorClass);
 			validator.initialize(annotation);
-			if(!validator.validate(controllerMethod,controllerInstance)){
+			if (!validator.validate(controllerMethod, controllerInstance)) {
 				return false;
 			}
 		}
 		return true;
 	}
 
-	public static List<Annotation> getCustomAcceptsAnnotations(Class<?> klass){
-		return new Mirror().on((AnnotatedElement) klass).reflectAll()
-				.annotations().matching(new AcceptsConstraintMatcher());
+	private static class AcceptsConstraintMatcher implements Matcher<Annotation> {
+		@Override
+		public boolean accepts(Annotation element) {
+			return element.annotationType().isAnnotationPresent(AcceptsConstraint.class);
+		}
 	}
 
+	public static List<Annotation> getCustomAcceptsAnnotations(Class<?> clazz){
+		return new Mirror().on((AnnotatedElement) clazz).reflectAll()
+				.annotations().matching(new AcceptsConstraintMatcher());
+	}
 }

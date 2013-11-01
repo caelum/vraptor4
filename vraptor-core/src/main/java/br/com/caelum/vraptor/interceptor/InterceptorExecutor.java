ab469132ff0f3e55271ff9cf7940991ba11f20ea
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-11-01 10:01:34 -0200

    javadoc for InterceptorExecutor

--- vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/InterceptorExecutor.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/interceptor/InterceptorExecutor.java
@@ -6,6 +6,17 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 
+import br.com.caelum.vraptor.AfterCall;
+import br.com.caelum.vraptor.AroundCall;
+import br.com.caelum.vraptor.BeforeCall;
+
+/**
+ * Simple executor for interceptor methods containing annotations:
+ * {@link BeforeCall}, {@link AroundCall} or {@link AfterCall}
+ *
+ * @author Rodrigo Turini
+ * @since 4.0
+ */
 @ApplicationScoped
 public class InterceptorExecutor {
 
@@ -28,12 +39,21 @@ public class InterceptorExecutor {
 		this.simpleInterceptorStack = simpleInterceptorStack;
 	}
 
+	/**
+	 * @param interceptor to be executed
+	 * @param method that should be annotated with {@link BeforeCall} or {@link AfterCall}
+	 */
 	public void execute(Object interceptor, Method method) {
 		if (method != null) {
 			stepInvoker.tryToInvoke(interceptor, method);
 		}
 	}
 
+	/**
+	 * <strong>note</strong>: Just for this case, method can receive DI.
+	 * @param interceptor to be executed
+	 * @param method that should be annotated with {@link AroundCall}.
+	 */
 	public void executeAround(Object interceptor, Method method) {
 		if (method != null) {
 			Object[] params = parametersResolver.parametersFor(method);

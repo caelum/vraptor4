5fdb5019fb47735fd69d02053184966f4ba04691
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-11-06 10:54:05 -0200

    creating new IncludeParameterEvent

---
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/events/IncludeParameterEvent.java
@@ -0,0 +1,14 @@
+package br.com.caelum.vraptor.events;
+
+import br.com.caelum.vraptor.interceptor.IncludeParameters;
+import br.com.caelum.vraptor.interceptor.ParametersInstantiatorInterceptor;
+
+/**
+ * Event fired by {@link ParametersInstantiatorInterceptor} when
+ * intercepted method has {@link IncludeParameters} annotation.
+ *
+ * @author Rodrigo Turini
+ * @since 4.0
+ */
+public class IncludeParameterEvent {
+}

8058c4cdb5db31fb6d5df32c3baabebece832ecf
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-11-08 15:17:14 -0200

    renaming OutjectResultEvent to MethodExecuted

---
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/events/MethodExecuted.java
@@ -0,0 +1,24 @@
+package br.com.caelum.vraptor.events;
+
+import br.com.caelum.vraptor.controller.ControllerMethod;
+import br.com.caelum.vraptor.interceptor.ExecuteMethodInterceptor;
+
+/**
+ * Event fired by {@link ExecuteMethodInterceptor}
+ * when it has fully completed it's execution.
+ *
+ * @author Rodrigo Turini
+ * @since 4.0
+ */
+public class MethodExecuted {
+
+	private ControllerMethod controllerMethod;
+
+	public MethodExecuted(ControllerMethod method) {
+		this.controllerMethod = method;
+	}
+
+	public ControllerMethod getControllerMethod() {
+		return controllerMethod;
+	}
+}

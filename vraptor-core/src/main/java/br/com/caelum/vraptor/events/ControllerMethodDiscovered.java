547235b5f7f753f31b28bf4d4bb293ea266bea53
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-11-13 15:18:15 -0200

    adding new ControllerMethodDiscovered event

---
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/events/ControllerMethodDiscovered.java
@@ -0,0 +1,22 @@
+package br.com.caelum.vraptor.events;
+
+import br.com.caelum.vraptor.controller.ControllerMethod;
+import br.com.caelum.vraptor.interceptor.ControllerLookupInterceptor;
+
+/**
+ * fired by {@link ControllerLookupInterceptor}
+ *
+ * @author Rodrigo Turini
+ */
+public class ControllerMethodDiscovered {
+
+	private ControllerMethod controllerMethod;
+
+	public ControllerMethodDiscovered(ControllerMethod method) {
+		this.controllerMethod = method;
+	}
+
+	public ControllerMethod getControllerMethod() {
+		return controllerMethod;
+	}
+}

3d264661a60bbdf5877ccba1841a4ec80997ea9b
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-11-14 09:04:04 -0200

    moving tests for observer package

--- vraptor-core/src/test/java/br/com/caelum/vraptor/observer/download/DownloadObserverTest.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/observer/download/DownloadObserverTest.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package br.com.caelum.vraptor.interceptor;
+package br.com.caelum.vraptor.observer.download;
 
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;

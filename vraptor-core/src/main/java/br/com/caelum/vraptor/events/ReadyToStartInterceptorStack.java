4086ff303e0f337f3026ed045399cdb482d8d3cd
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-11-26 09:51:59 -0200

    adding docs on ReadyToStartInterceptorStack

--- vraptor-core/src/main/java/br/com/caelum/vraptor/events/ReadyToStartInterceptorStack.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/events/ReadyToStartInterceptorStack.java
@@ -1,5 +1,13 @@
 package br.com.caelum.vraptor.events;
 
+import br.com.caelum.vraptor.core.DefaultInterceptorStack;
+
+/**
+ * Event fired by {@link DefaultInterceptorStack}
+ *
+ * @author Rodrigo Turini
+ * @since 4.0
+ */
 public class ReadyToStartInterceptorStack {
 
 }

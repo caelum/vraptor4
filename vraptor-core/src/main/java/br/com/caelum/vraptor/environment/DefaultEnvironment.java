370b1bf3ee760b7bd3eea3a618e9d848e3ad9af7
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2014-01-06 18:01:12 -0200

    using {} instead of %

--- vraptor-core/src/main/java/br/com/caelum/vraptor/environment/DefaultEnvironment.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/environment/DefaultEnvironment.java
@@ -54,7 +54,7 @@ public class DefaultEnvironment implements Environment {
             properties.load(stream);
             this.properties.putAll(properties);
         } else {
-            LOG.warn("Could not find the file '%s.properties' to load. If you ask for any property, null will be returned", environment);
+            LOG.warn("Could not find the file '{}.properties' to load. If you ask for any property, null will be returned", environment);
         }
     }
 

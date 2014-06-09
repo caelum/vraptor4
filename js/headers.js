fc08aaa2f0aab67d0ff8c8a6e37660e22ab40dac
Rodrigo Turini <rdrg.turini@gmail.com>
2014-06-09 13:42:40 -0300

    Updates

--- js/headers.js
+++ js/headers.js
@@ -1,7 +1,7 @@
 
 window.onload = function(){
 
-	var headers = document.querySelectorAll('.container h1, .container h2');
+	var headers = document.querySelectorAll('main.container h1, main.container h2');
 
 	for(var i = 0; i < headers.length; i++){
 	  var currentHeader = headers[i];

6bb00f3fb7c931f99197b6d5bdbb790969d9a09f
Renan <renanigt@gmail.com>
2014-06-15 03:02:50 -0300

    Removing some imports.

--- vraptor-musicjungle/src/test/java/br/com/caelum/vraptor/musicjungle/controller/HomeControllerTest.java
+++ vraptor-musicjungle/src/test/java/br/com/caelum/vraptor/musicjungle/controller/HomeControllerTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 

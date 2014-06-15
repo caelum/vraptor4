33a3ddb46cc1e61050c3e96d9517d135649605ba
Renan <renanigt@gmail.com>
2014-06-15 18:19:20 -0300

    Just returning to inital stage.

--- vraptor-core/src/test/java/br/com/caelum/vraptor/util/test/MockSerializationResultTest.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/util/test/MockSerializationResultTest.java
@@ -19,9 +19,19 @@ import static br.com.caelum.vraptor.view.Results.json;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import br.com.caelum.vraptor.proxy.JavassistProxifier;
+import br.com.caelum.vraptor.serialization.gson.GsonBuilderWrapper;
+import br.com.caelum.vraptor.serialization.xstream.XStreamBuilderFactory;
+
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonSerializer;
 
 public class MockSerializationResultTest {
 
@@ -29,7 +39,11 @@ public class MockSerializationResultTest {
 
 	@Before
 	public void setUp() throws Exception {
-		result = new MockSerializationResult();
+		List<JsonSerializer<?>> jsonSerializers = new ArrayList<>();
+		List<JsonDeserializer<?>> jsonDeserializers = new ArrayList<>();
+
+		result = new MockSerializationResult(new JavassistProxifier(), XStreamBuilderFactory.cleanInstance(),
+				new GsonBuilderWrapper(new MockInstanceImpl<>(jsonSerializers), new MockInstanceImpl<>(jsonDeserializers)));;
 	}
 
 	public static class Car {

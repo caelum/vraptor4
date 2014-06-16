590109d0c0b8288def05d01769c2a6fbfdaab054
Renan <renanigt@gmail.com>
2014-06-16 07:36:26 -0300

    Adding default constructor on MockSerializationResultTest.

--- vraptor-core/src/test/java/br/com/caelum/vraptor/util/test/MockSerializationResultTest.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/util/test/MockSerializationResultTest.java
@@ -19,19 +19,9 @@ import static br.com.caelum.vraptor.view.Results.json;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import br.com.caelum.vraptor.proxy.JavassistProxifier;
-import br.com.caelum.vraptor.serialization.gson.GsonBuilderWrapper;
-import br.com.caelum.vraptor.serialization.xstream.XStreamBuilderImpl;
-
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonSerializer;
 
 public class MockSerializationResultTest {
 
@@ -39,11 +29,7 @@ public class MockSerializationResultTest {
 
 	@Before
 	public void setUp() throws Exception {
-		List<JsonSerializer<?>> jsonSerializers = new ArrayList<>();
-		List<JsonDeserializer<?>> jsonDeserializers = new ArrayList<>();
-
-		result = new MockSerializationResult(new JavassistProxifier(), XStreamBuilderImpl.cleanInstance(),
-				new GsonBuilderWrapper(new MockInstanceImpl<>(jsonSerializers), new MockInstanceImpl<>(jsonDeserializers)));;
+		result = new MockSerializationResult();
 	}
 
 	public static class Car {

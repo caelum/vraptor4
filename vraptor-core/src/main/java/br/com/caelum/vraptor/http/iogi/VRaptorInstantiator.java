50177d35078ef44f3d256b2429b246d6fdf5abaf
Renan <renanigt@gmail.com>
2014-07-03 14:57:21 -0300

    Adding a test if the target is a List.

--- vraptor-core/src/main/java/br/com/caelum/vraptor/http/iogi/VRaptorInstantiator.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/http/iogi/VRaptorInstantiator.java
@@ -146,8 +146,12 @@ public class VRaptorInstantiator implements InstantiatorWithErrors, Instantiator
 		@Override
 		public Object instantiate(Target<?> target, Parameters parameters) {
 			try {
-				Parameter parameter = parameters.namedAfter(target);
-				return converterForTarget(target).convert(parameter.getValue(), target.getClassType());
+				if(!List.class.equals(target.getClassType())) {
+					Parameter parameter = parameters.namedAfter(target);
+					return converterForTarget(target).convert(parameter.getValue(), target.getClassType());
+				} else {
+					return multiInstantiator.instantiate(target, parameters);
+				}
 			} catch (ConversionException ex) {
 				errors.add(ex.getValidationMessage().withCategory(target.getName()));
 			} catch (IllegalStateException e) {

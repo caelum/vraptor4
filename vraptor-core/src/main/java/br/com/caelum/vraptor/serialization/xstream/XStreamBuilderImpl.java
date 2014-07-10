89dc64d330890a57aaded32b99280f31c04b3080
jlemes <jonatan@jslsolucoes.com.br>
2014-07-10 04:08:11 -0300

    Update XStreamBuilderImpl.java
    Relaxing recursive attribute to override MapWrapper in VRaptorXStream.class

--- vraptor-core/src/main/java/br/com/caelum/vraptor/serialization/xstream/XStreamBuilderImpl.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/serialization/xstream/XStreamBuilderImpl.java
@@ -39,7 +39,7 @@ public class XStreamBuilderImpl implements XStreamBuilder {
 	private final XStreamConverters converters;
 	private final TypeNameExtractor extractor;
 	private boolean indented = false;
-	private boolean recursive = false;
+	protected boolean recursive = false;
 	
 	/** 
 	 * @deprecated CDI eyes only

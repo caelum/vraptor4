46733cc0b1bbbc105377af48932b3b95623a5625
Renan <renanigt@gmail.com>
2014-07-29 01:28:12 -0300

    Adding Documented annotation.

--- vraptor-core/src/main/java/br/com/caelum/vraptor/WithoutRoot.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/WithoutRoot.java
@@ -16,11 +16,13 @@
 
 package br.com.caelum.vraptor;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Documented
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface WithoutRoot {

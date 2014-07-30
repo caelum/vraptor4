47b473661d560be0a29aaa01364e47832ad488b5
Renan <renanigt@gmail.com>
2014-07-30 02:12:04 -0300

    Adding Vetoed annotation on classes WithRoot and WithoutRoot.

--- vraptor-core/src/main/java/br/com/caelum/vraptor/serialization/gson/WithRoot.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/serialization/gson/WithRoot.java
@@ -1,8 +1,11 @@
 package br.com.caelum.vraptor.serialization.gson;
 
+import javax.enterprise.inject.Vetoed;
+
 import br.com.caelum.vraptor.serialization.Deserializee;
 import br.com.caelum.vraptor.serialization.DeserializerConfig;
 
+@Vetoed
 public class WithRoot implements DeserializerConfig {
 
 	@Override

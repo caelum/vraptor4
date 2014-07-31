38e6b9e7f24dc9800c6ccf876af43f5d1a7ff4ae
Renan <renanigt@gmail.com>
2014-07-31 09:04:19 -0300

    Adding docs on classes.

--- vraptor-core/src/main/java/br/com/caelum/vraptor/serialization/Deserializee.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/serialization/Deserializee.java
@@ -2,6 +2,15 @@ package br.com.caelum.vraptor.serialization;
 
 import javax.enterprise.context.Dependent;
 
+import br.com.caelum.vraptor.serialization.gson.GsonDeserialization;
+
+/**
+ * Used in {@link GsonDeserialization} to define some behaviors of deserializtion.
+ * 
+ * @author Renan Montenegro
+ * @since 4.1
+ *
+ */
 @Dependent
 public class Deserializee {
 
--- vraptor-core/src/main/java/br/com/caelum/vraptor/serialization/DeserializerConfig.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/serialization/DeserializerConfig.java
@@ -1,5 +1,16 @@
 package br.com.caelum.vraptor.serialization;
 
+import br.com.caelum.vraptor.Consumes;
+import br.com.caelum.vraptor.serialization.gson.GsonDeserialization;
+
+/**
+ * Used to {@link Consumes} options to configure {@link Deserializee} and change
+ * the {@link GsonDeserialization} behavior.
+ * 
+ * @author Renan Montenegro
+ * @since 4.1
+ *
+ */
 public interface DeserializerConfig {
 	void config(Deserializee deserializee);
 }

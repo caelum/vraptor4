384e501d45ba2cdcb21572918b43804920121c34
Rodrigo Turini <rdrg.turini@gmail.com>
2014-08-13 11:32:49 -0300

    updating VRaptor filter version

--- vraptor-core/src/main/java/br/com/caelum/vraptor/VRaptor.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/VRaptor.java
@@ -58,7 +58,7 @@ import br.com.caelum.vraptor.ioc.cdi.CDIRequestFactories;
 @WebFilter(filterName="vraptor", urlPatterns="/*", dispatcherTypes={DispatcherType.FORWARD, DispatcherType.REQUEST})
 public class VRaptor implements Filter {
 
-	public static final String VERSION = "4.1.0-RC2-SNAPSHOT";
+	public static final String VERSION = "4.1.0-RC2";
 
 	private final Logger logger = getLogger(VRaptor.class);
 

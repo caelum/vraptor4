c893207ce1062b559c1880005a957df2a7c59b5f
Otávio Garcia <otavio@otavio.com.br>
2014-08-31 00:26:29 -0300

    Adding tests when getting messages by severity

--- vraptor-core/src/test/java/br/com/caelum/vraptor/validator/MessagesTest.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/validator/MessagesTest.java
@@ -69,4 +69,22 @@ public class MessagesTest {
 		Collection<String> errors = ((MessageList) messages.getErrors()).from("client.name");
 		assertThat(errors.toString(), equalTo("not null, not empty"));
 	}
+
+	@Test
+	public void testReturnMessagesBySeverity() {
+		Message message0 = new SimpleMessage("client.id", "not null", Severity.ERROR);
+		Message message1 = new SimpleMessage("client.name", "not null", Severity.ERROR);
+		Message message2 = new SimpleMessage("client.email", "is valid", Severity.INFO);
+		Message message3 = new SimpleMessage("client.email", "was not checked", Severity.WARN);
+
+		messages.add(new SimpleMessage("client.id", "not null", Severity.ERROR));
+		messages.add(new SimpleMessage("client.name", "not null", Severity.ERROR));
+		messages.add(new SimpleMessage("client.email", "is valid", Severity.INFO));
+		messages.add(new SimpleMessage("client.email", "was not checked", Severity.WARN));
+
+		assertThat(messages.getErrors(), contains(message0, message1));
+		assertThat(messages.getInfo(), contains(message2));
+		assertThat(messages.getWarnings(), contains(message3));
+		assertThat(messages.getSuccess(), empty());
+	}
 }

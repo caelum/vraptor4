b333d29336bc1f54dbae1533cdd05d8fd6ada9a6
Rodrigo Turini <rdrg.turini@gmail.com>
2014-09-05 11:58:00 -0300

    starting contexts on setup

--- vraptor-core/src/test/java/br/com/caelum/vraptor/ioc/cdi/CDIBasedContainerTest.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/ioc/cdi/CDIBasedContainerTest.java
@@ -17,8 +17,6 @@ package br.com.caelum.vraptor.ioc.cdi;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.concurrent.Callable;
-
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
 
@@ -30,7 +28,6 @@ import br.com.caelum.cdi.component.UsingCacheComponent;
 import br.com.caelum.vraptor.WeldJunitRunner;
 import br.com.caelum.vraptor.events.VRaptorInitialized;
 import br.com.caelum.vraptor.ioc.GenericContainerTest;
-import br.com.caelum.vraptor.ioc.WhatToDo;
 
 @RunWith(WeldJunitRunner.class)
 public class CDIBasedContainerTest extends GenericContainerTest {
@@ -39,10 +36,11 @@ public class CDIBasedContainerTest extends GenericContainerTest {
 	@Inject private Event<VRaptorInitialized> initEvent;
 	@Inject private Contexts contexts;
 
-	private int counter;
-	
 	@Override
 	public void setup() throws Exception {
+		contexts.startRequestScope();
+		contexts.startSessionScope();
+		contexts.startApplicationScope();
 		initEvent.fire(new VRaptorInitialized(null));
 		super.setup();
 	}
@@ -53,30 +51,6 @@ public class CDIBasedContainerTest extends GenericContainerTest {
 		contexts.stopConversationScope();
 		contexts.stopSessionScope();
 		contexts.stopApplicationScope();
-	}
-
-	@Override
-	protected <T> T executeInsideRequest(final WhatToDo<T> execution) {
-		Callable<T> task = new Callable<T>() {
-			@Override
-			public T call() throws Exception {
-				contexts.startRequestScope();
-				contexts.startSessionScope();
-
-				T result = execution.execute(counter);
-
-				contexts.stopRequestScope();
-				contexts.stopSessionScope();
-
-				return result;
-			}
-		};
-		try {
-			T call = task.call();
-			return call;
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
 	}
 
 	@Test

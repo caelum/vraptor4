b57a88c462257a3de13fd002d302a480e5eb7f64
Otávio Garcia <otavio@otavio.com.br>
2014-10-02 13:42:42 -0300

    Rollback Servlet Environment exclusion

---
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/environment/ServletBasedEnvironment.java
@@ -0,0 +1,46 @@
+/***
+ * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package br.com.caelum.vraptor.environment;
+
+import java.io.IOException;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Alternative;
+import javax.servlet.ServletContext;
+
+/**
+ * A default {@link Environment} implementation which loads the environment file
+ * based on <i>VRAPTOR_ENV</i> system property or <i>br.com.caelum.vraptor
+ * .environment</i> property in the context init parameter.
+ * 
+ * @author Alexandre Atoji
+ * @author Andrew Kurauchi
+ * @author Guilherme Silveira
+ * @author Rodrigo Turini
+ * @deprecated Prefer using {@link DefaultEnvironment}.
+ */
+@ApplicationScoped
+@Alternative
+public class ServletBasedEnvironment extends DefaultEnvironment {
+
+	protected ServletBasedEnvironment() throws IOException {
+		super();
+	}
+
+	public ServletBasedEnvironment(ServletContext context) throws IOException {
+		super(context);
+	}
+}

d4c1c59e730d29a83d91b4a39646f687d63091c6
Otávio Garcia <otavio@otavio.com.br>
2014-10-22 16:58:38 -0200

    Adding overrides to compliance with jls

--- vraptor-core/src/main/java/br/com/caelum/vraptor/core/DefaultReflectionProvider.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/core/DefaultReflectionProvider.java
@@ -17,30 +17,37 @@ import net.vidageek.mirror.dsl.Mirror;
 @ApplicationScoped
 public class DefaultReflectionProvider implements ReflectionProvider {
 
+	@Override
 	public List<Method> getMethodsFor(Class<?> clazz) {
 		return new Mirror().on(clazz).reflectAll().methods();
 	}
 
+	@Override
 	public Method getMethod(Class<?> clazz, String methodName, Class<?>... parameterTypes) {
 		return new Mirror().on(clazz).reflect().method(methodName).withArgs(parameterTypes);
 	}
 
+	@Override
 	public Object invoke(Object instance, Method method, Object... args) {
 		return new Mirror().on(instance).invoke().method(method).withArgs(args);
 	}
 
+	@Override
 	public Object invoke(Object instance, String methodName, Object... args) {
 		return new Mirror().on(instance).invoke().method(methodName).withArgs(args);
 	}
 
+	@Override
 	public Object invokeGetter(Object instance, String fieldName) {
 		return new Mirror().on(instance).invoke().getterFor(fieldName);
 	}
 
+	@Override
 	public List<Field> getFieldsFor(Class<?> clazz) {
 		return new Mirror().on(clazz).reflectAll().fields();
 	}
 
+	@Override
 	public Field getField(Class<?> clazz, String fieldName) {
 		return new Mirror().on(clazz).reflect().field(fieldName);
 	}

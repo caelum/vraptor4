339fd123818f936ac1c1823b25d3700d682ed7e3
Rodrigo Turini <rdrg.turini@gmail.com>
2014-10-27 19:20:44 -0200

    injecting EMF instead creating manually

--- vraptor-musicjungle/src/main/java/br/com/caelum/vraptor/musicjungle/observer/InitialDataObserver.java
+++ vraptor-musicjungle/src/main/java/br/com/caelum/vraptor/musicjungle/observer/InitialDataObserver.java
@@ -2,9 +2,9 @@ package br.com.caelum.vraptor.musicjungle.observer;
 
 import javax.enterprise.context.Dependent;
 import javax.enterprise.event.Observes;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 import br.com.caelum.vraptor.events.VRaptorInitialized;
 import br.com.caelum.vraptor.musicjungle.model.User;
@@ -20,10 +20,16 @@ import br.com.caelum.vraptor.musicjungle.model.User;
  */
 @Dependent
 public class InitialDataObserver {
+	
+	private EntityManagerFactory factory;
+
+	@Inject 
+	public InitialDataObserver(EntityManagerFactory factory) {
+		this.factory = factory;
+	}
 
 	public void insert(@Observes VRaptorInitialized event) {
-		
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("default");
+
 		EntityManager manager = factory.createEntityManager();
 		manager.getTransaction().begin();
 		
@@ -35,6 +41,5 @@ public class InitialDataObserver {
 		manager.persist(defaultUser);
 		manager.getTransaction().commit();
 		manager.close(); 
-		factory.close();
 	}
 }

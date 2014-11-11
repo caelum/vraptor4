f7cd9035be9a373fb75f50a464094fd1ff018499
Francisco Sokol <chico.sokol@gmail.com>
2014-11-11 15:48:53 -0200

    Try should be generic

--- vraptor-core/src/main/java/br/com/caelum/vraptor/util/Try.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/util/Try.java
@@ -2,10 +2,10 @@ package br.com.caelum.vraptor.util;
 
 import java.util.concurrent.Callable;
 
-public abstract class Try {
-	public static Try run(Callable<Void> callable) {
+public abstract class Try<T> {
+	public static <T> Try run(Callable<T> callable) {
 		try {
-			Void call = callable.call();
+			T call = callable.call();
 			return new Success(call);
 		} catch (Exception e) {
 			return new Failed(e);
@@ -13,6 +13,8 @@ public abstract class Try {
 	}
 
 	public abstract boolean failed();
+
+	public abstract T value();
 
 	public abstract Exception getException();
 
@@ -26,6 +28,11 @@ public abstract class Try {
 		@Override
 		public boolean failed() {
 			return false;
+		}
+
+		@Override
+		public Object value() {
+			return result;
 		}
 
 		@Override
@@ -44,6 +51,11 @@ public abstract class Try {
 		@Override
 		public boolean failed() {
 			return true;
+		}
+
+		@Override
+		public Object value() {
+			throw new UnsupportedOperationException();
 		}
 
 		@Override

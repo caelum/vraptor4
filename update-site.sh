7d3b0cb010d54dfb907ab1b0657f490108b0bdcf
Luiz Fernando Oliveira Corte Real <sr.saude@gmail.com>
2015-01-27 16:18:22 -0200

    rebasing after updating

--- update-site.sh
+++ update-site.sh
@@ -6,8 +6,9 @@ bundle install
 rm -rf output deploy
 bundle exec nanoc
 grunt
-git checkout gh-pages
 cd ..
+git checkout gh-pages
 cp -R vraptor-site/deploy/* .
 git commit -am 'automatically updating vraptor site'
+git pull --rebase
 git checkout master

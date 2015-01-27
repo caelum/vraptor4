e069ed06b3049c2b55997f09971f6190cc83f9cf
Luiz Fernando Oliveira Corte Real <sr.saude@gmail.com>
2015-01-27 16:21:12 -0200

    halting on error & not going back to master in the end

--- update-site.sh
+++ update-site.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e # halt on error
+
 rvm use --create 2.0.0@vraptor-site
 cd vraptor-site
 bundle install
@@ -10,5 +12,3 @@ cd ..
 git checkout gh-pages
 cp -R vraptor-site/deploy/* .
 git commit -am 'automatically updating vraptor site'
-git pull --rebase
-git checkout master

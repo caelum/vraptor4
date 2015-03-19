3f05d6f87f97c5af9fc4871fc9774bdc01e64458
valeriolopes <valeriolopes@gmail.com>
2015-03-19 13:31:14 -0300

    Revert "fixed referer retrieval bug (if the context path string was present in"
    This reverts commit fa7065ccedcf527ba162d3817553ac044e734295.

--- vraptor-core/src/main/java/br/com/caelum/vraptor/view/DefaultRefererResult.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/view/DefaultRefererResult.java
@@ -21,8 +21,6 @@ import static br.com.caelum.vraptor.view.Results.logic;
 import static br.com.caelum.vraptor.view.Results.page;
 import static com.google.common.base.Preconditions.checkState;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 
 import javax.enterprise.context.RequestScoped;
@@ -94,19 +92,11 @@ public class DefaultRefererResult implements RefererResult {
 	}
 
 	private String getReferer() {
-	    String referer = request.getHeader("Referer");
-	    checkState(referer != null, "The Referer header was not specified");
-	    
-	    String refererPath = referer;
-		try {
-		    refererPath = new URL(referer).getPath();
-		} catch(MalformedURLException e) {
-		    //Maybe a relative path?
-		    refererPath = referer;
-		}
+		String referer = request.getHeader("Referer");
+		checkState(referer != null, "The Referer header was not specified");
 
 		String path = request.getContextPath();
-		return refererPath.substring(refererPath.indexOf(path) + path.length());
+		return referer.substring(referer.indexOf(path) + path.length());
 	}
 
 }

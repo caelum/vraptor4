bb5388fcd61a9b0e8b881e7bf78f9278ee24c783
Nykolas Lima <nykolas.lima@gmail.com>
2015-04-08 22:14:19 -0300

    loading properties in bean construction. This avoid concurrent access that could lead to errors in multithread environments

--- vraptor-core/src/main/java/br/com/caelum/vraptor/environment/DefaultEnvironment.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/environment/DefaultEnvironment.java
@@ -68,26 +68,21 @@ public class DefaultEnvironment implements Environment {
 	@Inject
 	public DefaultEnvironment(ServletContext context) {
 		this.context = context;
+		loadProperties();
 	}
 
 	public DefaultEnvironment(EnvironmentType environmentType) {
 		this((ServletContext) null);
 		this.environmentType = environmentType;
 	}
-
-	private Properties getProperties() {
-		if (properties == null) {
-			properties = new Properties();
-
-			loadAndPut(BASE_ENVIRONMENT_FILE);
-			loadAndPut(getEnvironmentType().getName());
-
-			LOG.debug("Environment is up with properties {}", properties);
-		}
-
-		return properties;
+	
+	private void loadProperties() {
+		properties = new Properties();
+		loadAndPut(BASE_ENVIRONMENT_FILE);
+		loadAndPut(getEnvironmentType().getName());
+		LOG.debug("Environment is up with properties {}", properties);
 	}
-
+	
 	private EnvironmentType getEnvironmentType() {
 		if (environmentType == null) {
 			environmentType = new EnvironmentType(findEnvironmentName(context));
@@ -165,7 +160,7 @@ public class DefaultEnvironment implements Environment {
 
 	@Override
 	public boolean has(String key) {
-		return getProperties().containsKey(key);
+		return properties.containsKey(key);
 	}
 
 	@Override
@@ -178,7 +173,7 @@ public class DefaultEnvironment implements Environment {
 		if (!isNullOrEmpty(systemProperty)) {
 			return systemProperty;
 		} else {
-			return getProperties().getProperty(key);
+			return properties.getProperty(key);
 		}
 	}
 
@@ -192,12 +187,12 @@ public class DefaultEnvironment implements Environment {
 
 	@Override
 	public void set(String key, String value) {
-		getProperties().setProperty(key, value);
+		properties.setProperty(key, value);
 	}
 
 	@Override
 	public Iterable<String> getKeys() {
-		return getProperties().stringPropertyNames();
+		return properties.stringPropertyNames();
 	}
 
 	@Override

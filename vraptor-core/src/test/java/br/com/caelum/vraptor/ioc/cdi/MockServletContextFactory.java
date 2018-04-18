8020976681203d9a89c214a55024ff3eb2a6f7b4
Daniel Badawi <daniel.badawi@preventsenior.com.br>
2018-04-18 17:46:18 -0300

    removing ServletContextFactory since Weld already provides ServletContext

--- vraptor-core/src/test/java/br/com/caelum/vraptor/ioc/cdi/MockServletContextFactory.java
+++ vraptor-core/src/test/java/br/com/caelum/vraptor/ioc/cdi/MockServletContextFactory.java
@@ -20,16 +20,14 @@ import static org.mockito.Mockito.mock;
 import javax.annotation.Priority;
 import javax.enterprise.inject.Alternative;
 import javax.enterprise.inject.Produces;
-import javax.enterprise.inject.Specializes;
 import javax.interceptor.Interceptor;
 import javax.servlet.ServletContext;
 
 @Alternative
 @Priority(Interceptor.Priority.LIBRARY_BEFORE)
-@Specializes
-class MockServletContextFactory extends ServletContextFactory {
+class MockServletContextFactory {
 
-	@Override @Produces
+    @Produces
 	public ServletContext getInstance() {
 		return mock(ServletContext.class);
 	}
